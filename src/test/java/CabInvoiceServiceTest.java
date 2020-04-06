5d1a0d37138ee83af3403414f0d8a0b846cdaa65
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-07 00:17:26 +0530

    ADDED: Test case to check multiple ride

--- src/test/java/CabInvoiceServiceTest.java
+++ src/test/java/CabInvoiceServiceTest.java
@@ -1,9 +1,18 @@
 import com.bridgelabz.service.CabInvoiceService;
+import com.bridgelabz.service.Ride;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.RowId;
+
 public class CabInvoiceServiceTest {
-    CabInvoiceService cabInvoiceService = new CabInvoiceService();
+    CabInvoiceService cabInvoiceService = null;
+
+    @Before
+    public void createObject() {
+        cabInvoiceService = new CabInvoiceService();
+    }
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
@@ -19,5 +28,16 @@ public class CabInvoiceServiceTest {
         int time = 2;
         double fare = cabInvoiceService.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1),
+                new Ride(4.1, 25)
+        };
+        double totalFare = cabInvoiceService.calculateFareForMultipleRides(rides);
+        Assert.assertEquals(96, totalFare, 0);
     }
 }
