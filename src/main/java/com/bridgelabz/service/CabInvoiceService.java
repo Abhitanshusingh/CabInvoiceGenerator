e17ea7a18b1b40310473746f3d7e4ed8dd1d51f2
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-06 17:42:27 +0530

    ADDED: Method to calculate fare of cab

--- src/main/java/com/bridgelabz/service/CabInvoiceService.java
+++ src/main/java/com/bridgelabz/service/CabInvoiceService.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.service;
 
 public class CabInvoiceService {
-    public static void main(String[] args) {
-        System.out.println("Welcome to cab invoice generator");
+    private static final double MAXUMUM_FARE = 5;
+    private static final double PER_KILOMETER_COST = 10;
+    private static final int PER_MINUTE_COST = 1;
+
+    public Double calculateFare(double distance, int time) {
+        double totalFare = (distance * PER_KILOMETER_COST) + (time * PER_MINUTE_COST);
+        if (totalFare < MAXUMUM_FARE) {
+            return MAXUMUM_FARE;
+        }
+        return totalFare;
     }
 }
