a432ac23cb48aa360f4bd135fd0ab514fcfd0e01
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-08 01:17:01 +0530

    ADDED: Methods to calculate no of rides total fare and average fare

--- src/main/java/com/bridgelabz/service/CabInvoiceService.java
+++ src/main/java/com/bridgelabz/service/CabInvoiceService.java
@@ -1,20 +1,38 @@
 package com.bridgelabz.service;
 
+import java.util.ArrayList;
+
 public class CabInvoiceService {
-    private static final double MAXUMUM_FARE = 5;
+    private static final double MINIMUM_FARE = 5;
     private static final double PER_KILOMETER_COST = 10;
     private static final int PER_MINUTE_COST = 1;
+    RideRepository rideRepository = new RideRepository();
+    ArrayList<Ride> listOfRides = new ArrayList<Ride>();
 
     public Double calculateFare(double distance, int time) {
         double totalFare = (distance * PER_KILOMETER_COST) + (time * PER_MINUTE_COST);
-        return Math.max(totalFare, MAXUMUM_FARE);
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 
     public double calculateFareForMultipleRides(Ride[] rides) {
         double aggregateFare = 0;
         for (Ride ride : rides) {
+            listOfRides.add(ride);
             aggregateFare += calculateFare(ride.distance, ride.time);
         }
         return aggregateFare;
+    }
+
+    public InvoiceSummary getInvoiceSummary(Ride[] rides) {
+        return new InvoiceSummary(rides.length, calculateFareForMultipleRides(rides));
+    }
+
+    public void addRides(String userId) {
+        rideRepository.addUserRides(userId, listOfRides);
+    }
+
+    public ArrayList<Ride> getRidesByUserId(String userId) {
+        ArrayList<Ride> ridesByUserId = rideRepository.getRidesByUserId(userId);
+        return ridesByUserId;
     }
 }
